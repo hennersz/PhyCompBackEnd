115292c8937689117ff94b01c30aa2f85d5b8daf
lujasmine <henry@morti.plus.com>
2016-04-13 16:19:47 +0100

    added new test data

--- test/test-server.js
+++ test/test-server.js
@@ -13,15 +13,57 @@ chai.use(chaiHttp);
 describe('API', function() {
 
   var collection = server.db.get('pollution');
-  var a = {
-    "_id":"56fab1838d45e3a41ac1795d",
-    "deviceID":2276,
-    "datetime":null,
-    "data":[{"no2":1},{"co":1}],
-    "loc":{"type":"Point","coordinates":[-0.127758299999982,51.5073509]}
-  }; 
+  var testData = [
+    {
+      "_id":"5702b14be4b4872d5d92b50b",
+      "datetime":"2015-03-25T20:00:38Z",
+      "data":[
+        {
+          "no2":{
+            "value":1,
+            "units":"AirQualityIndex",
+            "raw_value":0.01264,
+            "raw_units":"ppm"
+          }
+        },
+        {
+          "co":{
+            "value":1,
+            "units":"AirQualityIndex",
+            "raw_value":1.97912,
+            "raw_units":"ppm"
+          }
+        },
+        {
+          "light":{
+            "value":55.4,
+            "units":"Lux",
+            "raw_value":55.4,
+            "raw_units":"Lux"
+          }
+        },
+        {
+          "noise":{
+            "value":50,
+            "units":"dB",
+            "raw_value":50,
+            "raw_units":"dB"
+          }
+        },
+        {"no2":null},
+        {"so2":null},
+        {"o3":null},
+        {"pm10":null},
+        {"pm25":null},
+        {"co":null},
+        {"light":null},
+        {"noise":null}],"loc":{"type":"Point","coordinates":[-0.127758299999982,51.5073509]}}
+  ]; 
   beforeEach(function(done){
-    collection.insert(a);
+    for(i in testData)
+    {
+      collection.insert(testData[i]);
+    }
     collection.index({loc:"2dsphere"}).on('complete', function(err){done()});
   });
   afterEach(function(done){
@@ -34,6 +76,17 @@ describe('API', function() {
       .get('/api/all?latitude=51.5073&longitude=-0.1276&maxDist=200')
       .end(function(err, res) {
         res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        //checking that object has correct properties
+        res.body[0].should.have.property('_id');
+        res.body[0].should.have.property('datetime');
+        res.body[0].should.have.property('data');
+        res.body[0].should.have.property('loc');
+        res.body[0].loc.should.have.property('type');
+        res.body[0].loc.should.have.property('coordinates');
+        //checking object values are correct
+        
         done();
       });
   });

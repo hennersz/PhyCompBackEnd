cd854a17e1c5dade081330e60081e6b7ea8b0e37
Henry Mortimer <henry@morti.plus.com>
2016-04-20 17:13:51 +0100

    added tests for the small api requests

--- test/test-server.js
+++ test/test-server.js
@@ -70,6 +70,55 @@ describe('API', function() {
       });
     done();
   });
-  
+  it('should fail on out of range parameter values on /api/all/near', function(done) {
+    chai.request(server)
+      .get('/api/all/near?latitiude=300&longitude=-0.1276&maxDist=5000')
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+      });
+    chai.request(server)
+      .get('/api/all/near?latitiude=-300&longitude=-0.1276&maxDist=5000')
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+      });
+    chai.request(server)
+      .get('/api/all/near?latitiude=80&longitude=-999.1276&maxDist=5000')
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+      });
+    chai.request(server)
+      .get('/api/all/near?latitiude=80&longitude=999.1276&maxDist=5000')
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+      });
+    chai.request(server)
+      .get('/api/all/near?latitiude=80&longitude=9.1276&maxDist=-5000')
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+      });
+    done();
+  });
+  it('should give nearest value for all data types on /api/small', function(done) {
+    chai.request(server)
+      .get('/api/small?latitude=51.5072&longitude=-0.175')
+      .end(function(err,res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.have.property('no2');
+        res.body.should.have.property('co');
+        res.body.should.have.property('o3');
+        res.body.should.have.property('pm10');
+        res.body.should.have.property('so2');
+        res.body.should.have.property('pm25');
+        res.body.should.have.property('light');
+        res.body.should.have.property('noise');
+      });
+    done();
+  });
   
 });
